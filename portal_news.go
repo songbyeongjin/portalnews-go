8b98cf7918fc910523625d84e82bbd31920f25dd
sondol <song4489z@naver.com>
2020-04-29 21:19:32 +0900

    change router for using template block
    LoadHTMLGlob > router.HTMLRender

--- portal_news.go
+++ portal_news.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-contrib/multitemplate"
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 	"gopkg.in/yaml.v2"
@@ -13,38 +14,19 @@ import (
 )
 
 func main(){
-
 	// *** Set Db
-	dbConnector, err := getDbConnector()
-	if err != nil{
-		panic(err)
-	}
 
-	err = dbConnector.SetDbInstance()
-	if err != nil{
-		panic(err)
-	}
+	setDb()
 	defer db.Instance.Close()
-
-	db.Instance.LogMode(true)
 
 	// Set Db ***
 
 	// *** Set Router
 
-	f, _ := os.Create("./gin.log")
-	gin.DefaultWriter = io.MultiWriter(f,os.Stdout)
+	f, _ := os.Create("./server.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
 
-	router := gin.Default()
-
-
-	router.Static("/assets", "./assets")
-	router.LoadHTMLGlob("templates/*")
-
-	v1 := router.Group("/")
-	{
-		v1.GET("/", api_handler.Home)
-	}
+	router := setRouter()
 
 	// Set Router ***
 
@@ -52,7 +34,23 @@ func main(){
 	router.Run(":8080")
 }
 
+func setDb() {
+	dbConnector, err := getDbConnector()
+	if err != nil {
+		panic(err)
+	}
+
+	err = dbConnector.SetDbInstance()
+	if err != nil {
+		panic(err)
+	}
+
+	db.Instance.LogMode(true)
+}
+
+//Set Db information from yaml
 func getDbConnector() (*db.Connector, error){
+	//temporary path for debug mode
 	buf, err := ioutil.ReadFile(`C:\Users\SONG\Documents\study\go\src\portal_news\db_info.yaml`)
 	if err != nil {
 		fmt.Println(err)
@@ -68,4 +66,43 @@ func getDbConnector() (*db.Connector, error){
 	}
 
 	return connector, nil
+}
+
+
+// Set router
+func setRouter() *gin.Engine{
+	router := gin.Default()
+
+	//temporary path for debug mode
+	router.Static("/assets", `C:\Users\SONG\Documents\study\go\src\portal_news\assets`)
+
+	//temporary path for debug mode
+	router.LoadHTMLGlob(`C:\Users\SONG\Documents\study\go\src\portal_news\templates\*`)
+	router.HTMLRender = createRender()
+
+	//set default router
+	router.GET("/", api_handler.Home)
+
+	//set news group router
+	newsRouter := router.Group("/news")
+	{
+		newsRouter.GET("/", api_handler.Portal)
+		newsRouter.GET("/naver", api_handler.Naver)
+		newsRouter.GET("/nate", api_handler.Nate)
+		newsRouter.GET("/daum", api_handler.Daum)
+	}
+
+	return router
+}
+
+//Create render for using template block
+func createRender() multitemplate.Renderer {
+	rootPath := `C:\Users\SONG\Documents\study\go\src\portal_news\templates\`
+	headerPath := rootPath + `header.tmpl`
+
+	r := multitemplate.NewRenderer()
+	r.AddFromFiles("home", rootPath + `home.tmpl`, headerPath)
+	r.AddFromFiles("news", rootPath + `news.tmpl`, headerPath)
+
+	return r
 }
