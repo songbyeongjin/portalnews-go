19de389f71051264aac9319e7c44fb2d6e38acaa
sondol <song4489z@naver.com>
2020-04-30 04:40:49 +0900

    refactoring nate crawl

--- service/crawl_nate_news.go
+++ service/crawl_nate_news.go
@@ -21,12 +21,21 @@ const(
 	CssSelectorPress      = ".articleInfo .medium"
 	CssSelectorDate      = ".firstDate em"
 	CssSelectorOrCondition  = `, `
+	SetFieldCount = 4 //Title,Content,Press,Date
 )
 
 func CrawlNateNews(){
-	nateNewsUrls := make([]string,0,NewsCount)
-	nateNews := make([]model.News,NewsCount,NewsCount)
+	nateNewsUrls := getNateNewsUrls(NateNewsRootUrl)
+	nateNews := getNateNews(nateNewsUrls)
 
+	for _,news := range nateNews{
+		fmt.Println(news)
+	}
+}
+
+//get Nate News url From nate root url
+func getNateNewsUrls(nateRootUrl string) []string{
+	urls := make([]string,0,NewsCount)
 	c := colly.NewCollector()
 	var wg2 sync.WaitGroup
 	wg2.Add(20)
@@ -34,22 +43,27 @@ func CrawlNateNews(){
 	// Find and visit all links
 	c.OnHTML(CssSelectorFirstToFifth + CssSelectorOrCondition + CssSelectorSixthTo, func(e *colly.HTMLElement) {
 		//e.Request.Visit(e.Attr("href"))
-		if len(nateNewsUrls) < NewsCount{
-			nateNewsUrls = append(nateNewsUrls, e.Attr("href")[2:])
+		if len(urls) < NewsCount{
+			title := e.Attr("href")[2:]
+			urls = append(urls, title)
 			wg2.Done()
 		}
 	})
 
-	c.Visit(NateNewsRootUrl)
+	c.Visit(nateRootUrl)
 	wg2.Wait()
 
+	return urls
+}
 
-	fmt.Println(len(nateNewsUrls))
+//get NateNews Object from nate urls
+func getNateNews(nateNewsUrls []string) []model.News {
+	nateNews := make([]model.News, NewsCount, NewsCount)
 
 	cSlice := make([]*colly.Collector, NewsCount, NewsCount)
 	var wg sync.WaitGroup
 
-	for i:=0; i<NewsCount; i++{
+	for i := 0; i < NewsCount; i++ {
 		inIndex := i
 		cSlice[i] = colly.NewCollector()
 		cSlice[i].OnHTML(CssSelectorTitle, func(e *colly.HTMLElement) {
@@ -58,7 +72,7 @@ func CrawlNateNews(){
 		})
 		cSlice[i].OnHTML(CssSelectorContent, func(e *colly.HTMLElement) {
 			space := regexp.MustCompile(`\s+`)
-			str := space.ReplaceAllString(e.Text[:200] + "...", " ")
+			str := space.ReplaceAllString(e.Text[:200]+"...", " ")
 			nateNews[inIndex].Content = str
 
 			wg.Done()
@@ -73,21 +87,18 @@ func CrawlNateNews(){
 		})
 	}
 
-
-	for i, url := range  nateNewsUrls{
+	for i, url := range nateNewsUrls {
 		nateNews[i].Url = url
 		nateNews[i].Portal = "nate"
 		inUrl := url
 		inIndex := i
-		wg.Add(1 * 4)
-		go func(c2 *colly.Collector){
-			c2.Visit(HttpsUrl + inUrl)
-		}(cSlice[inIndex])// i+1 is ranking
+
+		wg.Add(1 * SetFieldCount)
+		go func(c *colly.Collector) {
+			c.Visit(HttpsUrl + inUrl)
+		}(cSlice[inIndex]) // i+1 is ranking
 	}
 	wg.Wait()
 
-	for _,news := range nateNews{
-		fmt.Println(news)
-	}
-
+	return nateNews
 }
