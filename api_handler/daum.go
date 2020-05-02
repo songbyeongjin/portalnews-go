acbd6d8c8eb72d711551a61ea07b805e0bccdda7
sondol <song4489z@naver.com>
2020-05-03 05:17:30 +0900

    add daum handler

--- api_handler/daum.go
+++ api_handler/daum.go
@@ -1,7 +1,16 @@
 package api_handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"portal_news/db"
+	"portal_news/model"
+)
 
 func Daum(c *gin.Context){
+	news := []model.RankingNews{}
 
+	db.Instance.Where("portal = ?", "daum").Find(&news)
+
+	c.HTML(http.StatusOK, "news", news)
 }
