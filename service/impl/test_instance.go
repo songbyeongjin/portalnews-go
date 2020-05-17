4b0b83eb3cf3def79182fe0ec3126d4520a7aa4d
sondol <song4489z@naver.com>
2020-05-18 02:31:49 +0900

    add test instance

---
+++ service/impl/test_instance.go
@@ -0,0 +1,21 @@
+package impl
+
+import (
+	"portal_news/infra/db"
+	"portal_news/infra/db/repository_impl"
+)
+
+var testPsDb = db.NewTestDbHandler() //use test DB
+
+var testUserR = repository_impl.NewUserRepository(testPsDb)
+var testNewsR = repository_impl.NewNewsRepository(testPsDb)
+var testReviewR = repository_impl.NewReviewRepository(testPsDb)
+var testRankingNewsR = repository_impl.NewRankingNewsRepository(testPsDb)
+
+
+var testLoginS = NewLoginService(testUserR)
+var testMyPageS = NewMyPageService(testReviewR, testNewsR)
+var testLogoutS = NewLogoutService()
+var testRankingNewsS = NewRankingNewsService(testRankingNewsR)
+var testUserS = NewUserService(testUserR)
+var testSearchS = NewSearchService(testNewsR)
